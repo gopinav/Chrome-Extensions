8418f1ddcee55ecb16ee27732f473d5b112b7667
Vishwas <gopinav5@gmail.com>
2016-08-20 08:22:59 +0530

    Add tts functionality

---
+++ Speak/eventPage.js
@@ -0,0 +1,18 @@
+var menuItem = {
+    "id": "Speak",
+    "title": "Speak",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){   
+    if (clickData.menuItemId == "Speak" && clickData.selectionText){        
+       chrome.tts.speak(clickData.selectionText,
+                        {
+                            'rate': 0.8
+                        });         
+    }
+});
+
+
